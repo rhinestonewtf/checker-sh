945f037c1510e9617aeec21e9c426600df1e8578
kopy-kat <kopp.konrad@gmail.com>
2024-12-26 13:50:32 +0100

    feat: add initial attestation checker for all modules

--- attestations.sh
+++ attestations.sh
@@ -1,11 +1,13 @@
 while IFS='=' read -r network url; do
     # Skip empty lines and comments
     [[ -z $network || $network == \#* ]] && continue
+
+    expanded_url=$(echo "$url" | envsubst)
     
     printf "\nProcessing network: $network\n"
 
     # Check that registry is deployed 
-    code=$(cast code 0x000000000069E2a187AEFFb852bF3cCdC95151B2 --rpc-url "$url")
+    code=$(cast code 0x000000000069E2a187AEFFb852bF3cCdC95151B2 --rpc-url "$expanded_url")
     if [ "$code" == "0x" ]; then
         printf '%s\n' "Error: Registry not deployed on $network" >&2
         exit 1
@@ -16,7 +18,7 @@ while IFS='=' read -r network url; do
         [[ -z $network || $network == \#* ]] && continue
     
         # Check module
-        code=$(cast call 0x000000000069E2a187AEFFb852bF3cCdC95151B2 "findAttestation(address,address)" $address 0x000000333034E9f539ce08819E12c1b8Cb29084d --rpc-url "$url")
+        code=$(cast call 0x000000000069E2a187AEFFb852bF3cCdC95151B2 "findAttestation(address,address)" $address 0x000000333034E9f539ce08819E12c1b8Cb29084d --rpc-url "$expanded_url")
         attestationTime=$(echo "$code" | cut -c 3-66)
         if [ "$attestationTime" == "0000000000000000000000000000000000000000000000000000000000000000" ]; then
             printf '%s\n' "Error: Attestation for $module not found on $network" >&2
