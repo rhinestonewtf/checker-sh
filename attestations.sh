1da2a45bebf3d5c885cdf7772cec00496ec6ac69
kopy-kat <kopp.konrad@gmail.com>
2025-01-07 22:01:23 +0100

    feat: allow providing specific networks or modules

--- attestations.sh
+++ attestations.sh
@@ -1,6 +1,30 @@
+# Store arguments in arrays
+networks=()
+modules=()
+while [[ $# -gt 0 ]]; do
+   case "$1" in
+       -n|--networks)
+           shift
+           while [[ $# -gt 0 && ! $1 =~ ^- ]]; do
+               networks+=("$1")
+               shift
+           done
+           ;;
+       -m|--modules)
+           shift
+           while [[ $# -gt 0 && ! $1 =~ ^- ]]; do
+               modules+=("$1")
+               shift
+           done
+           ;;
+   esac
+done
+
 while IFS='=' read -r network url; do
     # Skip empty lines and comments
     [[ -z $network || $network == \#* ]] && continue
+    # Skip if networks specified and current one not included
+   [[ ${#networks[@]} -gt 0 && ! " ${networks[@]} " =~ " ${network} " ]] && continue
 
     expanded_url=$(echo "$url" | envsubst)
     
@@ -15,7 +39,8 @@ while IFS='=' read -r network url; do
 
     while IFS='=' read -r module address; do
         # Skip empty lines and comments
-        [[ -z $network || $network == \#* ]] && continue
+        [[ -z $module || $module == \#* ]] && continue
+        [[ ${#modules[@]} -gt 0 && ! " ${modules[@]} " =~ " ${module} " ]] && continue
     
         # Check module
         attestation=$(cast call 0x000000000069E2a187AEFFb852bF3cCdC95151B2 "findAttestation(address,address)" $address 0x000000333034E9f539ce08819E12c1b8Cb29084d --rpc-url "$expanded_url")
